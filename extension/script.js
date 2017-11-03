5247529d63fa874a33f07fc4b9ffe5b5518227ab
Laszlo Heves <laszlo_heves@discovery.com>
2017-11-03 17:52:47 +0000

    update redirect to gothamist

--- extension/script.js
+++ extension/script.js
@@ -1,23 +1,9 @@
-(() => {
-  const getUrl = () => {
-    return [
-      document.location.protocol,
-      '//',
-      'nol.hu',
-      document.location.pathname
-    ].join('');
-  };
-
-  const fuckNer = () => {
-    document.getElementsByTagName('h1')[0].textContent = 'Fuck NER!';
-    document.getElementsByTagName('p')[0].textContent = 'megyünk az archívumba';
-  };
-
-  const gotoWayback = (url) => {
-    document.location = url;
-  };
-
-  const url = getUrl();
-  fuckNer();
-  gotoWayback(`http://web.archive.org/web/${url}`);
-})();
+chrome.webRequest.onBeforeRequest.addListener((e) => {
+  return {
+    redirectUrl: `http://web.archive.org/web/${e.url}`
+  }
+}, {urls: [
+      "*://*.gothamist.com/*",
+      "*://gothamist.com/*"
+    ],types: ["main_frame"]
+}, ["blocking"]);
